dd27f48b0e56b4ef92714742edd14e7b592a0518
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-05-08 23:09:49 +0300

    show command is adapted to database

--- main/java/server/commands/ShowCommand.java
+++ main/java/server/commands/ShowCommand.java
@@ -2,14 +2,15 @@ package server.commands;
 
 import org.example.collections.Dragon;
 import org.example.tools.OutputText;
+import server.database.DataBaseStuds;
 import server.manager.ObjectsCollectionManager;
 import server.manager.ObjectsElements;
 import server.manager.ObjectsManager;
 import server.modules.ServerSender;
-import server.tools.Sort;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.sql.Driver;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,14 +29,15 @@ public class ShowCommand implements Command {
         ObjectsManager objectsManager = new ObjectsManager();
         ObjectsElements objectsElements = new ObjectsElements();
         ObjectsCollectionManager getters = new ObjectsCollectionManager();
+        List<Dragon> dragonList = new LinkedList<>(getters.getAll());
 
         if (objectsManager.length() > 0) {
             for (int i = 0; i < objectsManager.length(); i++) {
-                list.add(objectsElements.element(getters.getDragonByIndex(i), command));
+                if (args[0].equals(dragonList.get(i).getUserLogin())) {
+                    list.add(objectsElements.element(getters.getDragonByIndex(i), command));
+                }
             }
-            return new ServerSender<>(list);
-        } else {
-            return new ServerSender<>(List.of(OutputText.result("Empty")));
         }
+        return list.size() > 0 ? new ServerSender<>(list) : new ServerSender<>(List.of(OutputText.result("Empty")));
     }
 }
